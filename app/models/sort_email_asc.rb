507533d81ebfabf4f34f359979ab78997ef753c3
Artur Bersan de Faria <arturbersan@gmail.com>
2016-11-29 12:31:47 -0200

    Fixing sintaxe error of method sort_email

--- app/models/sort_email_asc.rb
+++ app/models/sort_email_asc.rb
@@ -1,7 +1,7 @@
 class SortEmailAsc < SortStrategy
 
   def sort_email(users)
-    users.sort! { |a,b| a.email <==> b.email }
+    users.sort! { |a,b| a.email <=> b.email }
   end
 
 end
--- app/models/sort_email_desc.rb
+++ app/models/sort_email_desc.rb
@@ -1,7 +1,7 @@
 class SortEmailDesc < SortStrategy
 
   def sort_email(users)
-    users.sort! { |a,b| b.email <==> a.email }
+    users.sort! { |a,b| b.email <=> a.email }
   end
 
 end
